5fefb72ab507e00563863c6d78e463fcdbe88b4b
ebenezerowusu <eo199124@gmail.com>
2023-03-16 09:53:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ *Main - Comment
+ *Betty Style
+ */
 int main () {
 
-   puts("\"Programming is like building a multilingual puzzle\n");
+   puts("\"Programming is like building a multilingual puzzle");
    
    return(0);
 }
