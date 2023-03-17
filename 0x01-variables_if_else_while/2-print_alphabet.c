159cc17d679979dd14dbcaf904c1a6f562cccbc5
ebenezerowusu <eo199124@gmail.com>
2023-03-17 09:42:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success);
+ */
+int main(void)
+{
+	int char = 'abcdefghijklmnopqrstuvwxyz';
+	putchar(char);
+	return(0);
+}
