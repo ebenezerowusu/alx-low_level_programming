d2d0e78347d4dd1f7d60de382eb1a2e92c30a108
ebenezerowusu <eo199124@gmail.com>
2023-03-17 10:58:28 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
         int c;
 
         for (c = 'a'; c <= 'z'; c++)
-        {
-                if (c == 'e' && c == 'q')
-                        continue;
-        }
-                putchar(c);
+	{
+		if (c == 'e' && c == 'q')
+			continue;
+		putchar(c);
+	}
         putchar('\n');
         return (0);
 }
