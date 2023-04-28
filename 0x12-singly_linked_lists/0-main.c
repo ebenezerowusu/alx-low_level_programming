6511c7d605a98deec78eb068cf00812ed393b73f
Owusu Ebenezer <38473854+ebenezerowusu@users.noreply.github.com>
2023-04-28 02:06:50 +0000

    0-main.c

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -5,17 +5,20 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
 {
+	size_t n;
+	list_t hello = {"World", 5, NULL};
 	list_t *head;
 	list_t *new;
-	list_t hello = {"World", 5, NULL};
-	size_t n;
+	
 
-	head = &hello;
+	
 	new = malloc(sizeof(list_t));
+	head = &hello;
 	if (new == NULL)
 	{
 		printf("Error\n");
