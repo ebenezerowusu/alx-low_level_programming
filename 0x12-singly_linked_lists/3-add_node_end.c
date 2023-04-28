579ea9460c00c4c8cf99cf9528dbcba46f42984d
Owusu Ebenezer <38473854+ebenezerowusu@users.noreply.github.com>
2023-04-28 21:09:22 +0000

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,7 +14,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new; 
 	list_t *tem = *head;
 	
-	unsigned int len = 0;
+	
 
 	new = malloc(sizeof(list_t));
 	
@@ -22,7 +22,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	
 	new->str = strdup(str);
-	new->len = len;
+	new->len = strlen(str);
 	new->next = NULL;
 
 	if (tem != NULL)
