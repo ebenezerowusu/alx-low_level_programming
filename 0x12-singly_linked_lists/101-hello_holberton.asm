62a96a441b6df6087dacc03d8d221fbce5763ae2
Owusu Ebenezer <38473854+ebenezerowusu@users.noreply.github.com>
2023-04-28 21:31:34 +0000

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,12 +2,14 @@
 msg:	db "Hello, Holberton", 0
 fmt:	db "%s", 10, 0
 
-global    main
+	SECTION .text
 	extern printf
 	global main
 main:
-	 mov   edi, format
-	 xor   eax, eax
-	 call  printf
-	 mov 	eax, 0
-	 ret
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
