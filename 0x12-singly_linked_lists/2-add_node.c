0f2656e3606a0cc9efd219873ffb12317e88c4b3
Owusu Ebenezer <38473854+ebenezerowusu@users.noreply.github.com>
2023-04-28 02:32:23 +0000

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,7 +5,7 @@
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: head pointer.
- * @str: string to store in the list.
+ * @str: string to store in the new node.
  * Return: address of the head or return null.
  */
 list_t *add_node(list_t **head, const char *str)
