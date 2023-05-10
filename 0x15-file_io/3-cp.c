f14f3cc7790f3f2a66ed814ece26c4e961e4d5ef
Owusu Ebenezer <38473854+ebenezerowusu@users.noreply.github.com>
2023-05-10 04:57:50 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,6 +7,7 @@
  * @argv: array of arguments
  * Return: 0 on success, 97-100 on failure
  */
+
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, read_bytes, write_bytes;
@@ -16,14 +17,13 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	fd_from = open(argv[1], O_RDONLY);
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
